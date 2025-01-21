05195dc494bcd41d25cb83cdc7fe1b8536a48241
dimashidyat <77518929+dimashidyat@users.noreply.github.com>
2025-01-21 08:14:16 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# Config buat bot lo
+TOKEN = "7563675945:AAE7iiaL7ry0NfNZv_TSYZoyaN6fLAbqHdg"
+CHAT_ID = "2031898002"
+
+# Info pribadi
+MY_PHONE = "+6281776633344"
+GIRLFRIEND_PHONE = "+6281513607410"
+
+# Channel buat latihan soal
+CHANNELS = {
+    "bumn": "@latihansoalbumn2025"
+}
+ 
